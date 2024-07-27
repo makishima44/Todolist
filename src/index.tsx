567c50970c4ccf9ba82eae0d0a18ea927ae18f64
Artur <metallist15431543@gmail.com>
2024-07-27 12:38:17 +0300

    05/01 start

--- src/index.tsx
+++ src/index.tsx
@@ -1,56 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import App from "./app/App";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import { TodolistsList } from "./features/TodolistsList/TodolistsList";
-import { Login } from "./features/Login/Login";
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
-import { ErrorPage } from "./components/ErrorPage/ErrorPage";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './app/App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Navigate to={"/404"} />,
-    children: [
-      {
-        index: true,
-        element: <Navigate to="/todolists" />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/todolists",
-        element: <TodolistsList />,
-      },
-    ],
-  },
-  {
-    path: "/404",
-    element: <ErrorPage />,
-  },
-]);
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
+const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
+	<Provider store={store}>
+		<App/>
+	</Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
